3182ca16c52b514181792f39b86bb3e26cfc5c09
peteryouseph <jose_p@ufms.br>
2023-11-17 16:58:29 -0400

    feat: includes functions unlink, rmdir, rename, chmod, chown e truncate to myfs.c

--- src/myfs.c
+++ src/myfs.c
@@ -183,6 +183,19 @@ static int do_mknod(const char *path, mode_t mode, dev_t rdev)
     return 0;
 }
 
+int get_dir_index(const char *dirname)
+{
+    for (int i = 0; i <= curr_dir_idx; i++)
+    {
+        if (strcmp(dir_list[i], dirname) == 0)
+        {
+            return i;
+        }
+    }
+
+    return -1; // Return -1 if the directory was not found
+}
+
 static int do_write(const char *path, const char *buffer, size_t size, off_t offset, struct fuse_file_info *info)
 {
     write_to_file(path, buffer);
@@ -190,6 +203,101 @@ static int do_write(const char *path, const char *buffer, size_t size, off_t off
     return size;
 }
 
+void remove_file(const char *filename)
+{
+    int file_idx = get_file_index(filename);
+
+    if (file_idx == -1)
+        return;
+
+    for (int i = file_idx; i < curr_file_idx; i++)
+    {
+        strcpy(files_list[i], files_list[i + 1]);
+        strcpy(files_content[i], files_content[i + 1]);
+    }
+
+    curr_file_idx--;
+    curr_file_content_idx--;
+}
+
+void remove_dir(const char *dirname)
+{
+    int dir_idx = get_dir_index(dirname);
+
+    if (dir_idx == -1)
+        return;
+
+    for (int i = dir_idx; i < curr_dir_idx; i++)
+    {
+        strcpy(dir_list[i], dir_list[i + 1]);
+    }
+
+    curr_dir_idx--;
+}
+
+static int do_unlink(const char *path)
+{
+    path++;
+    remove_file(path);
+
+    return 0;
+}
+
+static int do_rmdir(const char *path)
+{
+    path++;
+    remove_dir(path);
+
+    return 0;
+}
+
+static int do_rename(const char *from, const char *to)
+{
+    int file_idx = get_file_index(from);
+
+    if (file_idx != -1)
+    {
+        strcpy(files_list[file_idx], to + 1);
+        return 0;
+    }
+
+    int dir_idx = get_dir_index(from);
+
+    if (dir_idx != -1)
+    {
+        strcpy(dir_list[dir_idx], to + 1);
+        return 0;
+    }
+
+    return -ENOENT;
+}
+
+static int do_chmod(const char *path, mode_t mode)
+{
+    // This is a no-op in our simple filesystem.
+    return 0;
+}
+
+static int do_chown(const char *path, uid_t uid, gid_t gid)
+{
+    // This is a no-op in our simple filesystem.
+    return 0;
+}
+
+static int do_truncate(const char *path, off_t size)
+{
+    int file_idx = get_file_index(path);
+
+    if (file_idx == -1)
+        return -ENOENT;
+
+    memset(files_content[file_idx], 0, 256);
+    strncpy(files_content[file_idx], files_content[file_idx], size);
+
+    return 0;
+}
+
+// Add these to your fuse_operations struct
 static struct fuse_operations operations = {
     .getattr = do_getattr,
     .readdir = do_readdir,
@@ -197,6 +305,12 @@ static struct fuse_operations operations = {
     .mkdir = do_mkdir,
     .mknod = do_mknod,
     .write = do_write,
+    .unlink = do_unlink,
+    .rmdir = do_rmdir,
+    .rename = do_rename,
+    .chmod = do_chmod,
+    .chown = do_chown,
+    .truncate = do_truncate,
 };
 
 int main(int argc, char *argv[])
